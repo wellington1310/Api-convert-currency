546bdbb00b944e3ed469bddb31a4117639b5442b
Wellington Eduardo <101137533+wellington1310@users.noreply.github.com>
2023-02-22 18:54:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 // app.use("/", express.static("https://www.youtube.com/watch?v=WqdmgnScCgc"))
 
-app.ger("/" , (req,res)=>{
+app.get("/" , (req,res)=>{
     res.send("foi")
 } 
     )
