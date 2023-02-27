27e2eb13d616c5f8f1c820e88768309b2d3746e1
Wellington Eduardo <wellingtoneduardo16@outlook.com>
2023-02-27 17:28:42 -0300

    mudei o Cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 
 app.use( cors({
-    origin: "https://convert-currency-wellington.netlify.app/"
+    origin: "https://convert-currency-wellington.netlify.app"
 }))
 
 
